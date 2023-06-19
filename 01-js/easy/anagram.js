152c8d40b03e63429e744ee48c8b65348fdb57e8
Shubh2025 <bham2051@gmail.com>
2023-06-19 17:27:03 +0530

    made changes in anagram code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+   
+  if(str1.split("").sort().join("") == str2.split("").sort().join("")){
+    return true;
+  }
+  else
+    return false;
 }
+
+var ans=isAnagram("abcc","cbca");
+console.log(ans);
 
 module.exports = isAnagram;
